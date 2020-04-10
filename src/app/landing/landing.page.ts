065dcef0da5436e69dfec4c592a2c0ac91c826a7
Chris Guilliams <chris.guilliams@modea.com>
2020-04-10 12:12:25 -0400

    toasts now appear above gamble button on homepage

--- src/app/landing/landing.page.ts
+++ src/app/landing/landing.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RandomNumberService } from '../services/random-number.service';
+import { ToastController } from '@ionic/angular';
+import { createAnimation } from '@ionic/core';
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private randomNumberService: RandomNumberService,
+    private toastController: ToastController) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+  }
+
+  async gamble() {
+    let earnings = this.randomNumberService.getRandomNumber(-50, 50);
+    await this.showEarningsToast(earnings);
+  }
+
+  async presentEarningsToast(earnings: number) {
+    const cssClasses = 'earnings-toast' + ((earnings > 0) ? ' positive' : ' negative');
+    const earningsMessage = ((earnings > 0) ? ' Earned: ' : ' Lost: ') + earnings + ' creds';
+    const toast = await this.toastController.create({
+      message: earningsMessage,
+      position: 'bottom',
+      cssClass: cssClasses
+    });
+    toast.present();
+  }
+
+  async showEarningsToast(earnings: number) {
+    try {
+      this.toastController.dismiss();
+    } finally {
+      await this.presentEarningsToast(earnings);
+    }
   }
 
 }
--- src/app/services/random-number.service.ts
+++ src/app/services/random-number.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+declare let BigNumber;
 @Injectable({
   providedIn: 'root'
 })
 export class RandomNumberService {
 
   constructor() { }
+
+  getRandomNumber(min: number, max: number) {
+    return Math.floor(BigNumber.random() * (max - min) + min);
+  }
+
 }
